8db7309bacb497112637d460267ce822458dff5f
ADMIN <ADMIN@ADMIN-PC>
2019-09-20 12:41:58 +0700

    Latihan-Java

---
+++ src/pbo11/pkg10118910/latihan4/PBO1110118910Latihan4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11.pkg10118910.latihan4;
+
+    /**
+ *
+ * @author 
+ * NAMA : Muhamad Syifa Amrulloh 
+ * KELAS : PBO11K 
+ * NIM : 10118910
+ *
+ * Description Program : Program ini Berisi pengunaan method Kambing
+ */
+public class PBO1110118910Latihan4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void tambahKambing() {
+        //Deklarasi variabel lokal
+        int JumlahKambing = 0;
+
+        JumlahKambing = JumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + JumlahKambing);
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBO1110118910Latihan4 Kambingjantan = new PBO1110118910Latihan4();
+        Kambingjantan.tambahKambing();
+    }
+
+}
